c26008c0ac172e96282751bfcd828745d4c28641
0x0desha74 <mudesha74@gmail.com>
2025-03-10 01:08:57 +0200

    06 - Path Variable, 07- Request Param

--- src/main/java/in/springrestapi/Controllers/EmployeesController.java
+++ src/main/java/in/springrestapi/Controllers/EmployeesController.java
@@ -5,13 +5,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
-@RestController //@Controller + @ResponseBody
+@RestController ///@Controller + @ResponseBody
+@RequestMapping(value = "/api")
 public class EmployeesController {
 
-    //GET : /employees
-    @GetMapping(value = "/employees")
+    //GET : /api/employees
+    @GetMapping( "/employees")
     public String getEmployees() {
         return "List of employees";
     }
+
+    //GET : /api/employee/1
+
+@GetMapping("/employees/{id}")
+    public String getEmployee(@PathVariable int id){
+        return "employee with id: " + id;
+}
+
+// DELETE : /api/employees?id=1&name=Mustafa
+@DeleteMapping("/employees")
+public String deleteEmployee(@RequestParam int id,@RequestParam String name ){
+        return "employee with id: " + id + " and name: " + name ;
+}
 
 }
