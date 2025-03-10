37d4224bc29dbffe428887e817c08da4aa33a3d2
0x0desha74 <mudesha74@gmail.com>
2025-03-10 19:24:23 +0200

    @Value Annotation

--- src/main/java/in/springrestapi/Controllers/EmployeesController.java
+++ src/main/java/in/springrestapi/Controllers/EmployeesController.java
@@ -2,42 +2,56 @@
 
 
     import in.springrestapi.Models.Employee;
+    import org.springframework.beans.factory.annotation.Value;
     import org.springframework.stereotype.Controller;
     import org.springframework.web.bind.annotation.*;
 
 
-    @RestController ///@Controller + @ResponseBody
+    @RestController
+    ///@Controller + @ResponseBody
     @RequestMapping(value = "/api")
     public class EmployeesController {
 
         //GET : /api/employees
-        @GetMapping( "/employees")
+        @GetMapping("/employees")
         public String getEmployees() {
             return "List of employees";
         }
 
-        //GET : /api/employee/1
+        @Value("${app.name}")
+        private String appName;
+        @Value("${app.version}")
+        private String appVersion;
 
-    @GetMapping("/employees/{id}")
-        public String getEmployee(@PathVariable int id){
-            return "employee with id : " + id;
-    }
-
-    @PostMapping("/employees")
-    public String addEmployee(@RequestBody Employee employee){
-            return  employee.toString();
-    }
-
-        @PutMapping("/employees")
-        public Employee editEmployee(@RequestBody Employee employee){
-            return  employee;
+        @GetMapping("/version")
+        public String getVersion() {
+            return appName + " - " + appVersion;
         }
 
 
-    // DELETE : /api/employees?id=1&name=Mustafa
-    @DeleteMapping("/employees")
-    public String deleteEmployee(@RequestParam int id,@RequestParam String name ){
-            return "employee with id: " + id + " and name: " + name ;
-    }
+        //GET : /api/employee/1
+
+        @GetMapping("/employees/{id}")
+        public String getEmployee(@PathVariable int id) {
+            return "employee with id : " + id;
+        }
+
+        @PostMapping("/employees")
+        public String addEmployee(@RequestBody Employee employee) {
+            return employee.toString();
+        }
+
+        @PutMapping("/employees/{id}")
+        public Employee editEmployee(@PathVariable int id, @RequestBody Employee employee) {
+            System.out.println("Update employee with id: " + id);
+            return employee;
+        }
+
+
+        // DELETE : /api/employees?id=1&name=Mustafa
+        @DeleteMapping("/employees")
+        public String deleteEmployee(@RequestParam int id, @RequestParam String name) {
+            return "employee with id: " + id + " and name: " + name;
+        }
 
     }
